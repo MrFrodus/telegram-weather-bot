98214074f2f22d9b69096bb8a953b5c62cd18ea5
MrFrodus <gulashiko220@gmail.com>
2023-08-30 10:50:22 +0300

    D:/progs/Git/start command, options after entering text, handling unknown command

--- index.ts
+++ index.ts
@@ -2,8 +2,37 @@ import TelegramBot from "node-telegram-bot-api";
 
 const token = "6524276387:AAFoKHzWnV5VNtDpUHYoBInI70S2SUyUf78";
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", msg => {
-    console.log(msg);
-})
+bot.setMyCommands([{ command: "/start", description: "Start the bot" }]);
+
+bot.on("message", async (msg) => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
+
+  console.log(msg);
+
+  if (text === "/start") {
+    return await bot.sendMessage(
+      chatId,
+      "Welcome, please enter the name of the city"
+    );
+  }
+
+  if (!msg.entities && text) {
+    return await bot.sendMessage(chatId, msg.text!, {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Forecast for tomorrow", callback_data: "1" }],
+          [{ text: "Forecast for 3 days", callback_data: "3" }],
+          [{ text: "Forecast for the next week", callback_data: "7" }],
+        ],
+      },
+    });
+  }
+
+  return await bot.sendMessage(
+    chatId,
+    "I don't have this command, please enter the city name to get the forecast"
+  );
+});
