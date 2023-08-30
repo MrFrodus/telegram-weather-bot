d36bff96e3cd17e7345fcd0f19023aded6784ced
MrFrodus <gulashiko220@gmail.com>
2023-08-30 13:35:25 +0300

    getting weather data via axios, on callback query sending message with weather data

--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
 import TelegramBot from "node-telegram-bot-api";
+import axios from "axios";
 
 const token = "6524276387:AAFoKHzWnV5VNtDpUHYoBInI70S2SUyUf78";
 
 const bot = new TelegramBot(token, { polling: true });
 
 bot.setMyCommands([{ command: "/start", description: "Start the bot" }]);
+
+const getWeather = async (city: string, days: number) => {
+  try {
+    const { data } = await axios.get(
+      "http://api.weatherapi.com/v1/forecast.json?",
+      {
+        params: { q: city, days, key: "8e41c12f8c6148b1b0b151654232908" },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 bot.on("message", async (msg) => {
   const text = msg.text;
@@ -23,7 +39,7 @@ bot.on("message", async (msg) => {
     return await bot.sendMessage(chatId, msg.text!, {
       reply_markup: {
         inline_keyboard: [
-          [{ text: "Forecast for tomorrow", callback_data: "1" }],
+          [{ text: "Forecast for tomorrow", callback_data: "2" }],
           [{ text: "Forecast for 3 days", callback_data: "3" }],
           [{ text: "Forecast for the next week", callback_data: "7" }],
         ],
@@ -35,4 +51,46 @@ bot.on("message", async (msg) => {
     chatId,
     "I don't have this command, please enter the city name to get the forecast"
   );
+});
+
+bot.on("callback_query", async (msg) => {
+  const days = Number(msg.data);
+  const chatId = msg.message!.chat.id;
+  const text = msg.message!.text;
+
+  const weatherData = await getWeather(text!, days);
+  bot.sendMessage(
+    chatId,
+    `Forecast for ` +
+      (days === 2
+        ? `the next day`
+        : days === 3
+        ? `the next three days`
+        : days === 7
+        ? `the next week`
+        : "")
+  );
+  if (days === 2) {
+    return bot.sendMessage(
+      chatId,
+      `Date: ${weatherData.forecast.forecastday[1].date} 
+    \nMax Temperature: ${weatherData.forecast.forecastday[1].day.maxtemp_c}
+    \nMin Temperature: ${weatherData.forecast.forecastday[1].day.mintemp_c}
+    \nAvg Temperature: ${weatherData.forecast.forecastday[1].day.avgtemp_c}
+    \nHumidity: ${weatherData.forecast.forecastday[1].day.avghumidity}
+    \nHumidity: ${weatherData.forecast.forecastday[1].day.avghumidity}`
+    );
+  } 
+
+  weatherData.forecast.forecastday.map((day: any) => {
+    return bot.sendMessage(
+      chatId,
+      `Date: ${day.date} 
+      \nMax Temperature: ${day.day.maxtemp_c}
+      \nMin Temperature: ${day.day.mintemp_c}
+      \nAvg Temperature: ${day.day.avgtemp_c}
+      \nHumidity: ${day.day.avghumidity}
+      \nHumidity: ${day.day.avghumidity}`
+    );
+  });
 });
